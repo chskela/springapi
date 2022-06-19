3e8181cb91d1236f1c8870a77f6dde16910f0d17
chskela <chskela@gmail.com>
2022-06-19 20:42:05 +0300

    Added BankService

---
+++ src/main/kotlin/com/chskela/springapi/service/BankService.kt
@@ -0,0 +1,11 @@
+package com.chskela.springapi.service
+
+import com.chskela.springapi.datasource.BankDataSource
+import com.chskela.springapi.models.Bank
+import org.springframework.stereotype.Service
+
+@Service
+class BankService(private val dataSource: BankDataSource) {
+
+    fun getBanks(): Collection<Bank> = dataSource.retrieveBanks()
+}
