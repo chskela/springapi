4d9f5041f7acfe7a696389a0a325b277c9123d3c
chskela <chskela@gmail.com>
2022-06-19 20:42:23 +0300

    Added BankServiceTest

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,8 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.mockk:mockk:1.12.4")
 }
 
 tasks.withType<KotlinCompile> {
