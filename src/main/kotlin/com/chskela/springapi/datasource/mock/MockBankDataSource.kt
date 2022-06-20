0348ef658f681d00db592e60d882448688199fa3
chskela <chskela@gmail.com>
2022-06-20 21:53:38 +0300

    Update MockBankDataSource - add @Repository("mock")

--- src/main/kotlin/com/chskela/springapi/datasource/mock/MockBankDataSource.kt
+++ src/main/kotlin/com/chskela/springapi/datasource/mock/MockBankDataSource.kt
@@ -2,12 +2,14 @@ package com.chskela.springapi.datasource.mock
 
 import com.chskela.springapi.datasource.BankDataSource
 import com.chskela.springapi.models.Bank
+import org.springframework.stereotype.Repository
 
+@Repository("mock")
 class MockBankDataSource : BankDataSource {
     private val banks = listOf(
         Bank("1234", 10.0, 4),
-        Bank("1234", 3.14, 3),
-        Bank("1234", 3.14, 17),
+        Bank("34535", 3.14, 3),
+        Bank("1345345354", 3.14, 17),
     )
 
     override fun retrieveBanks(): Collection<Bank> = banks
